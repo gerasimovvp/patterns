1ea394fe28ada3caa3aaa22c314b9c1471159247
Vitaly Gerasimov <cyber_vit@mail.ru>
2025-05-08 23:22:53 +0300

    chapter 9, iterator

---
+++ Chapter 8/BeverageTestDrive.java
@@ -0,0 +1,12 @@
+public class BeverageTestDrive {
+    public static void main(String[] args) {
+        TeaWithHook teaHook = new TeaWithHook();
+        CoffeeWithHook coffeeHook = new CoffeeWithHook();
+
+        System.out.println("Making tea");
+        teaHook.prepareRecipe();
+
+        System.out.println("Making coffee");
+        coffeeHook.prepareRecipe();
+    }
+}
---
+++ Chapter 9/PancakeHouseMenu.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+
+public class PancakeHouseMenu{
+    ArrayList<MenuItem> menuItems;
+
+    public PancakeHouseMenu(){
+        menuItems = new ArrayList<MenuItem>();
+
+        addItem("K&B's Pancake breakfast", "Pancakes with scrambled eggs, and toast", true, 2.99);
+        addItem("Regular Pancake breakfast", "Pancakes with fried eggs, sausage", false, 2.99);
+        addItem("Blueberry pancakes", "Pancakes made with fresh blueberries", true, 3.49);
+        addItem("Waffles", "Waffles, with your choice of blueberries or strawberries", true, 3.59);
+    }
+
+    public void addItem(String name, String description, boolean vegetarian, double price){
+        MenuItem menuItem = new MenuItem(name, description, vegetarian, price);
+        menuItems.add(menuItem);
+    }
+
+    // public ArrayList<MenuItem> getMenuItems(){
+    //     return menuItems;
+    // }
+
+    public Iterator createIterator(){
+        return new PancakeHouseIterator(menuItems);
+    }
+}
